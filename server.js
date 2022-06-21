f9e65f320d841d360ee14854a5e30f5f63295a36
JtheFox <60980739+JtheFox@users.noreply.github.com>
2022-06-21 15:56:15 -0400

    add get and post for notes

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+const uuid = require('./utils/uuid');
+const notesDB = require('./db/db.json');
 
-const app = express();
 const PORT = 3001;
+const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -21,12 +23,18 @@ app.get('/notes', (req, res) => {
 
 // fetch notes from db
 app.get('/api/notes', (req, res) => {
-
+    res.json(notesDB);
 });
 
 // add notes to db
 app.post('/api/notes', (req, res) => {
-
+    fs.writeFile(
+        path.join(__dirname, '/db/db.json'),
+        JSON.stringify([...notesDB, { ...req.body, id: uuid() }], null, 2),
+        (err) => {
+            if (err) console.error(err);
+            else console.log('Notes updated successfully.');
+        });
 });
 
 // start server
