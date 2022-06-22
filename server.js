d2c1cc3237952e5a53a99c28123f3a45fa59615f
JtheFox <60980739+JtheFox@users.noreply.github.com>
2022-06-22 11:05:41 -0400

    minor update to wording

--- server.js
+++ server.js
@@ -1,26 +1,28 @@
+// imports
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const uuid = require('./utils/uuid');
 const dbPath = path.join(__dirname, './db/db.json');
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
+// db functions
 const dbOps = {
     read() { return JSON.parse(fs.readFileSync(dbPath)) },
     add(db, note) {
         try {
-            fs.writeFileSync(dbPath, JSON.stringify([...db, { ...note, id: uuid() }], null, 2));
+            fs.writeFileSync(dbPath, JSON.stringify(db.push({ ...note, id: uuid() }), null, 2));
             return 'Successfully updated db';
         } catch {
             return new Error('Error writing to db');
         }
-    }, 
+    },
     delete(db, id) {
         try {
             fs.writeFileSync(dbPath, JSON.stringify(db.filter(note => note.id !== id), null, 2));
@@ -31,30 +33,20 @@ const dbOps = {
     }
 }
 
-// landing page
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/index.html'))
-});
+// GET Route for landing page
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
 
-// notes page
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/notes.html'))
-});
+// GET Route for notes page
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
 
-// fetch notes from db
-app.get('/api/notes', (req, res) => {
-    res.json(dbOps.read());
-});
+// GET Route to fetch notes from db
+app.get('/api/notes', (req, res) => res.json(dbOps.read()));
 
-// add note to db
-app.post('/api/notes', (req, res) => {
-    console.log(dbOps.add(dbOps.read(), req.body));
-});
+// POST Route to add note to db
+app.post('/api/notes', (req, res) => console.log(dbOps.add(dbOps.read(), req.body)));
 
-// delete note from db
-app.delete('/api/notes/:id', (req, res) => {
-    console.log(dbOps.delete(dbOps.read(), req.params.id));
-});
+// DELETE Route to delete note from db
+app.delete('/api/notes/:id', (req, res) => console.log(dbOps.delete(dbOps.read(), req.params.id)));
 
 
 // start server
